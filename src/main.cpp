348406b622700e76da29775f2070d8f3d6472798
harveyPhung <tuongquang.phung@outlook.com>
2022-08-17 22:29:19 +1000

    config::src update files for testing

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "sensors.h"
+#include <iostream>
+
+int32_t main() {
+  std::cout << "Sensors Installation:" << std::endl;
+  std::cout << "-------------------------------------" << std::endl;
+
+  return -1;
+}
